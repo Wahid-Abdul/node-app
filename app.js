2889fdf9b299250e2731f7a77cbc57ee2f66a5cc
Wahid-Abdul <abdu14101.cs@rmkec.ac.in>
2018-07-28 22:11:14 +0530

    changing to app.js

---
+++ app.js
@@ -0,0 +1,34 @@
+var express = require('express');
+var mongoose = require("mongoose");
+var bodyParser     =        require("body-parser");
+
+
+
+
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+
+
+
+
+
+
+app.get('/', function(req, res){
+    res.send("Base URl");
+ });
+ 
+app.get('/hello', function(req, res){
+    res.send({'name':'abdul wahid'});
+    console.log("REQ:"+req[0])
+});
+
+app.post('/hello',function(req,res){
+    res.send(res.body);
+    console.log( res.body)    
+
+});
+
+app.listen(3000);
