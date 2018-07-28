7b201242fa4e81adf0f02cf0cd3d151532acf52c
Wahid-Abdul <abdu14101.cs@rmkec.ac.in>
2018-07-29 00:23:44 +0530

    mongoD in the picture

--- app.js
+++ app.js
@@ -1,16 +1,32 @@
 var express = require('express');
-var mongoose = require("mongoose");
 var bodyParser = require("body-parser");
-
-
-
+var MongoClient = require('mongodb').MongoClient;
 
 var app = express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+var url = "mongodb://localhost:27017/mydb";
 
+MongoClient.connect(url, { useNewUrlParser: true } , function(err, db) {
+      if (err) throw err;
+      console.log("Database created!");
+
+      var dbo  =  db.db("firsty")
+      dbo.createCollection("names",function (err,res) {
+        if(err) throw err;
+        console.log("\n\n\n\n\nCollection created"+    (res));
+        var myobj = { name: "Company Inc", address: "Highway 37" };
+        dbo.collection("names").insertOne(myobj, function(err, res) {
+            if (err) throw err;
+            console.log("1 document inserted");
+            db.close();
+        });
+      });
+
+     
+    });
 
 
 
@@ -26,8 +42,9 @@ app.get('/hello', function(req, res){
 });
 
 app.post('/hello',function(req,res){
-    (res.send(res.body));
     console.log( req.body)    
+
+    res.send(req.body);
 
 });
 
